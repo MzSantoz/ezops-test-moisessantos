7a8a5f979a2b0ae6aea76f4edeaa857f8d1570cd
Moises Santos <mzsantoz@outlook.com>
2020-09-09 18:16:34 -0300

    First commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+#Selected the node version image to start.
+FROM node:12
+#selected where in the container the app files should be.
+WORKDIR /usr/src/ezops
+#Copy all the packages in the root folder to install dependencies after.
+COPY package*.json ./
+#The line above helps provide faster, reliable, reproducible builds for production environments.
+RUN npm ci --only=production
+#Copy the source code to the image.
+COPY . .
+#expose the app port
+EXPOSE 8080
+#Finally, the command to run the app.
+CMD [ "node", "index.js" ]
---
+++ index.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+const Message = mongoose.model('Message',{
+  name : String,
+  message : String
+});
+
+app.get('/messages', (_, res) => {
+  Message.find({},(err, messages)=> {
+    res.send(messages);
+  })
+});
+
+
+app.get('/messages/:user', (req, res) => {
+  const user = req.params.user;
+  Message.find({name: user},(err, messages)=> {
+    res.send(messages);
+  })
+});
+
+
+app.post('/messages', async (req, res) => {
+  try{
+    const message = new Message(req.body);
+    const savedMessage = await message.save()
+      console.log('saved');
+
+    const censored = await Message.findOne({message:'badword'});
+      if(censored)
+        await Message.remove({_id: censored.id})
+      else
+        io.emit('message', req.body);
+      res.sendStatus(200);
+  }
+  catch (error){
+    res.sendStatus(500);
+    return console.log('error',error);
+  }
+  finally{
+    console.log('Message Posted')
+  }
+
+});
+
+io.on('connection', (socket) =>{
+  console.log('a user is connected');
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+// Connection to mongodb atlas
+
+const DB_CONNECTION = "mongodb+srv://mz-master:mz1475963@cluster0.kbdpt.mongodb.net/ezops-admission-test?retryWrites=true&w=majority";
+
+console.log('Iniciando conexão ao MongoDB...');
+mongoose.connect(
+  DB_CONNECTION,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  (err) => {
+    if (err) {
+      connectedToMongoDB = false;
+      console.error(`Erro na conexão ao MongoDB - ${err}`);
+    }
+  }
+);
+
+const { connection } = mongoose;
+
+connection.once('open', () => {
+  connectedToMongoDB = true;
+  console.log('Conectado ao MongoDB');
+  const port = process.env.PORT || 8080;
+
+  http.listen(port, () => {
+    console.log(`server is running on port => ${port}`);
+  });
+});
